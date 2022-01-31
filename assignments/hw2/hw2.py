9bcb309ebc8ff1c387b829be4a5c38b370736ece
Ben <kelehearbz@g.cofc.edu>
2022-01-30 20:22:57 -0500

    Homework 2

--- assignments/hw2/hw2.py
+++ assignments/hw2/hw2.py
@@ -1,35 +1,60 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Ben Kelehear
+hw2.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Homework 2
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+
 import math
 
 
 def sum_of_threes():
-    pass
+    upper_bound = eval(input("What is the upper bound?: "))
+    my_range = range(3, upper_bound + 1, 3)
+    print("Sum of threes is: ", list(my_range))
+
 
 
 def multiplication_table():
-    pass
+    for i in range(1, 11):
+        for j in range(1, 11):
+            print(i * j, end="\t")
+    print()
+
 
 
 def triangle_area():
-    pass
+    a = eval(input("Enter side a length: "))
+    b = eval(input("Enter side b length: "))
+    c = eval(input("Enter side c length: "))
+    s = a + b + c / 2
+    area = math.sqrt(s * s - a * s - b * s - c)
+    print("The area of your triangle is: ", area, ":)")
 
 
 def sum_squares():
-    pass
+    lower= eval(input("Please enter the lower range: "))
+    upper = eval(input("Please enter the upper range: "))
+    n = 1
+    for i in range(lower, upper):
+        n = base * n
+    print(n)
+
+
 
 
 def power():
-    pass
+    base = eval(input("Hello! Please enter your base number: "))
+    exponent = eval(input("Now, enter your exponent: "))
+    n = 1
+    for i in range(n, exponent+1):
+        n = base * n
+    print(base, "^", exponent, "is: ", n)
+
+
 
 
 if __name__ == '__main__':
