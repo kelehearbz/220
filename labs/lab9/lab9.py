f37a2c1966ca7cc8456522068ad7f116c39fb36d
Ben <kelehearbz@g.cofc.edu>
2022-03-28 21:27:11 -0400

    Lab 9

--- labs/lab9/lab9.py
+++ labs/lab9/lab9.py
@@ -5,7 +5,8 @@ Name: <your name goes here – first and last>
 
 
 def build_board():
-    pass
+    board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    return board
 
 
 def print_board(board):
@@ -35,33 +36,77 @@ def print_board(board):
     print(row_3)
     print(reset)
 
-
 def is_legal(board, position):
-    pass
+    legal = str(board[position-1]).isnumeric()
+    return legal
 
 
 def fill_spot(board, position, character):
-    pass
+    character = character.strip().lower()
+    board[position-1] = character
 
 
 def winning_game(board):
-    pass
+    for i in range(0, 8, 3):
+        if board[i] == board[i + 1] == board[i + 2]:
+            return True
+    for j in range(0, 3, 1):
+        if board[j] == board[j + 3] == board[j + 6]:
+            return True
+    if board[0] == board[4] == board[8]:
+        return True
+    if board [2] == board[4] == board[6]:
+        return True
+    else:
+        return False
 
 
 def game_over(board):
-    pass
+    if winning_game(board):
+        return True
+    for i in range(1, 10):
+        if is_legal(board, i):
+            return False
+    return True
 
 
 def get_winner(board):
-    pass
+    if winning_game(board):
+        xCount = 0
+        oCount = 0
+
+        for position in board:
+            if position == 'x':
+                xCount += 1
+            elif position == 'o':
+                oCount += 1
+        if xCount == oCount:
+            return 'o'
+        else:
+            return 'x'
+    else:
+        return None
+
 
 
 def play(board):
-    pass
+    playing = True
+    print_board(board)
+    while playing:
+        if fill_spot(board):
+            print("Sorry! That space is already taken")
+        if game_over(board):
+            playing = False
 
 
 def main():
-    pass
+    play_again = True
+    while play_again == True:
+        again = input("Good game! Do you want to play again? ")
+        if again == 'y':
+            play()
+        elif again == 'n':
+            print("Thanks for playing! See you later! :)")
 
 
 if __name__ == '__main__':
