aa1b9197d413beec4e2c6dc13c0551ddc7877f40
Ben <kelehearbz@g.cofc.edu>
2022-04-02 23:10:20 -0400

    Homework 9

--- assignments/hw9/hw9.py
+++ assignments/hw9/hw9.py
@@ -7,44 +7,71 @@ Problem: <Brief, one or two sentence description of the problem that this progra
 Certification of Authenticity:
 I certify that this assignment is entirely my own work.
 """
+from random import randint
+
 
 def get_words(file_name):
     file = open(file_name, "r")
     file_read = file.readlines()
-    file_string = file_read.str()
-    print(file_string)
+    return file_read
 get_words("hangman.txt")
 
 
-# def get_random_word(words):
-#     pass
-#
-#
-# def letter_in_secret_word(letter, secret_word):
-#     pass
-#
-#
-# def already_guessed(letter, guesses):
-#     pass
-#
-#
-# def make_hidden_secret(secret_word, guesses):
-#     pass
-#
-#
-# def won(guessed):
-#     pass
-#
-#
-# def play_graphics(secret_word):
-#     pass
-#
-#
-# def play_command_line(secret_word):
-#     pass
-#
-#
-# if __name__ == '__main__':
-#     pass
-#     # play_command_line(secret_word)
-#     # play_graphics(secret_word)
+def get_random_word(words):
+    file = open(words, "r")
+    file_read = file.readlines()
+    print(file_read)
+
+get_random_word("hangman.txt")
+    
+
+def letter_in_secret_word(letter, secret_word):
+        file = open(letter, "r")
+        file_read = file.readlines()
+        print(file_read)
+
+get_random_word("hangman.txt")
+
+
+def already_guessed(letter, guesses):
+    file = open(letter, "r")
+    file_read = file.readlines()
+    print(file_read)
+
+get_random_word("hangman.txt")
+
+
+def make_hidden_secret(secret_word, guesses):
+    file = open(words, "r")
+    file_read = file.readlines()
+    print(file_read)
+
+get_random_word("hangman.txt")
+
+
+def won(guessed):
+    file = open(guessed, "r")
+    file_read = file.readlines()
+    print(file_read)
+
+get_random_word("hangman.txt")
+
+
+def play_graphics(secret_word):
+    file = open(secret_word, "r")
+    file_read = file.readlines()
+    print(file_read)
+
+get_random_word("hangman.txt")
+
+
+def play_command_line(secret_word):
+    file = open(secret_word, "r")
+    file_read = file.readlines()
+    print(file_read)
+
+get_random_word("hangman.txt")
+
+if __name__ == '__main__':
+    play_command_line(secret_word)
+    play_graphics(secret_word)
